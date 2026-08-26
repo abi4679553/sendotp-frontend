3e2264d45d49ccdb590319ac47d7bdb82491b12c
abi4679553 <abivengadajalam7708@gmail.com>
2026-08-26 21:21:57 +0530

    header

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+        <p>ABI</p>
+        <p>Home</p>
+        <p>About</p>
+        <p>Contact</p>
+        <p>Login</p>
+        <p>Sign up</p>
+
+    </div>
+  )
+}
+
+export default Header
