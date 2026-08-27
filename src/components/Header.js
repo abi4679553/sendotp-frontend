7629c8179bf864838955da6b43e277360e1e3bf5
abi4679553 <abivengadajalam7708@gmail.com>
2026-08-27 10:12:49 +0530

    header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,13 +2,15 @@ import React from 'react'
 
 function Header() {
   return (
-    <div>
+    <div className='flex bg-sky-800 text-white gap-4 p-3 '>
         <p>ABI</p>
+        <div className='flex gap-10 ml-[70%]'>
         <p>Home</p>
         <p>About</p>
         <p>Contact</p>
         <p>Login</p>
         <p>Sign up</p>
+        </div>
 
     </div>
   )
